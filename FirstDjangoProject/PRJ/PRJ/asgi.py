8722472123ba8fe22c769f1af7de09bf0b94f5d1
Zardiny <Zare_zardiny@yahoo.com>
2023-11-10 22:58:08 +0330

    For Test

--- FirstDjangoProject/PRJ/PRJ/asgi.py
+++ FirstDjangoProject/PRJ/PRJ/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for PRJ2 project.
+ASGI config for PRJ project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PRJ2.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PRJ.settings')
 
 application = get_asgi_application()
--- FirstDjangoProject/PRJ/PRJ/urls.py
+++ FirstDjangoProject/PRJ/PRJ/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for PRJ2 project.
+URL configuration for PRJ project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
